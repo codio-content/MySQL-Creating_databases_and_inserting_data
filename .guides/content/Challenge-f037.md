2e2ed25502da35c99fd2157fb00db82da11e37d4
netpoe <yo@soygus.com>
2015-09-10 16:39:30 +0000

    Challenge 10.1 sec

--- .guides/content/Challenge-f037.md
+++ .guides/content/Challenge-f037.md
@@ -1,9 +1,19 @@
-
+{Check It!|assessment}(test-3260325949)
 
 |||guidance
-###Correct answers:
-1. 
-2. 
-3.
+### Correct answers:
+
+1. `USE famous_scientists;`
+2.
+```
+CREATE TABLE scientists (
+  id INT(4) NOT NULL auto_increment,
+  name VARCHAR(255) NOT NULL,
+  invention TEXT NOT NULL,
+  year_of_birth YEAR(4) NOT NULL,
+  year_of_death YEAR(4) DEFAULT NULL,
+  PRIMARY KEY (id)
+) auto_increment = 1;
+```
 
 |||
--- .guides/sqltests.js/sql-2-5.js
+++ .guides/sqltests.js/sql-2-5.js
@@ -7,12 +7,19 @@ var workspace = sqltest.workspaceDirectory;
  tasks[{Task description | Expected Query}]
 */
 var tasks = [
-
- ];
+  [
+    "Use the famous_scientists database",
+    "USE famous_scientists"
+  ],
+  [
+    "Create a scientists table with the specified features",
+    "CREATE TABLE scientists (id INT(4) NOT NULL auto_increment, name VARCHAR(255) NOT NULL, invention TEXT NOT NULL, year_of_birth YEAR(4) NOT NULL, year_of_death YEAR(4) DEFAULT NULL, PRIMARY KEY (id)) auto_increment = 1"
+  ]
+];
 
 
 /*
  Init test
  sql.testCommands(user source file, database name, tasks array)
 */
-sqltest.testCommands("sql-2-5/sql-2-5.sql", "people", tasks);
+sqltest.testCommands("sql-2-5/sql-2-5.sql", "famous_scientists", tasks, "scientists");

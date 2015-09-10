4682eb5a5fe74b663a32634676c55829d1709a05
netpoe <yo@soygus.com>
2015-09-10 17:15:54 +0000

    10.3. challenge sec

---
+++ .guides/content/Challenge-7abf.md
@@ -0,0 +1,13 @@
+{Check It!|assessment}(test-1856823852)
+
+
+|||guidance
+### Correct answers:
+
+1. `USE famous_scientists;`
+2. `SHOW TABLES;`
+3. `SELECT * FROM scientists;`
+4. `SELECT name,directory FROM scientists ORDER BY name DESC;`
+5. `SELECT * FROM scientists WHERE year_of_birth > 1500 ORDER BY year_of_birth DESC;`
+
+|||
--- .guides/sqltests.js/sql-2-7.js
+++ .guides/sqltests.js/sql-2-7.js
@@ -7,12 +7,31 @@ var workspace = sqltest.workspaceDirectory;
  tasks[{Task description | Expected Query}]
 */
 var tasks = [
-
- ];
+  [
+    "Use the famous_scientists schema",
+    "USE famous_scientists"
+  ],
+  [
+    "Show the existing tables",
+    "SHOW TABLES"
+  ],
+  [
+    "Select al the records from the scientists table",
+    "SELECT * FROM scientists"
+  ],
+  [
+    "Select the name and discovery columns of the scientists table and sort them by name in descending order",
+    "SELECT name,directory FROM scientists ORDER BY name DESC"
+  ],
+  [
+    "Select all the records from the scientists table where year_of_birth is greater than 1500. Sort them by year_of_birth in descending order",
+    "SELECT * FROM scientists WHERE year_of_birth > 1500 ORDER BY year_of_birth DESC"
+  ]
+];
 
 
 /*
  Init test
  sql.testCommands(user source file, database name, tasks array)
 */
-sqltest.testCommands("sql-2-7/sql-2-7.sql", "people", tasks);
+sqltest.testCommands("sql-2-7/sql-2-7.sql", "famous_scientists", tasks);
